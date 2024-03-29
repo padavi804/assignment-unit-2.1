b13fd29442b460936c1907bafa011ede98ab193c
Patrick Davison <davisonpatrickj@gmail.com>
2024-03-29 16:02:05 -0500

    added name and number

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,15 +1,19 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Patrick';
+console.log(firstName)
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Davison'
+console.log(lastName)
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-
+let fullName = (firstName + lastName)
+console.log(fullName)
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
-
+luckyNumber = 8
+console.log(luckyNumber)
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
